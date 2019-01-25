a7de2b851bcab59574db3e4ddf7d9bd757f8a95c
ycngu <snl123698745@163.com>
2019-01-25 21:53:21 +0800

    v1.5

--- src/js/initializers/av.js
+++ src/js/initializers/av.js
@@ -1,9 +1,9 @@
-fs = require('fs')
+// fs = require('fs')
 var APP_ID = 'BpRDxrD5ekiyXXc7ruHL9ooy-gzGzoHsz';
 var APP_KEY = '60kbTMFODU37TGXG0XwuAx8q';
 
-var key = fs.readFileSync('../../leancloud.json')
-var {APP_ID, APP_KEY} = JSON.parse(key)
+// var key = fs.readFileSync('../../leancloud.json')
+// var {APP_ID, APP_KEY} = JSON.parse(key)
 
 AV.init({
   appId: APP_ID,
--- src/js/song-list.js
+++ src/js/song-list.js
@@ -5,44 +5,72 @@
       <ul class="songList">
       </ul>
     `,
-    render(data){
+    render(data) {
       let $el = $(this.el)
       $el.html(this.template)
-      let {songs} = data
-      let liList = songs.map((song)=> 
+      let {
+        songs
+      } = data
+      let liList = songs.map((song) =>
         $('<li></li>').text(song.name)
       )
       $el.find('ul').empty()
-      liList.map((domLi)=>{
+      liList.map((domLi) => {
         $el.find('ul').append(domLi)
       })
     },
-    clearActive(){
+    clearActive() {
       $(this.el).find('.active').removeClass('active')
     }
   }
   let model = {
     data: {
-      songs: [ ]
+      songs: []
+    },
+    find() {
+      var query = new AV.Query('Song')
+      return query.find().then((songs) => {
+        this.data.songs = songs.map((song) => {
+          return {
+            id: song.id,
+            ...song.attributes
+          }
+        })
+        return songs
+      })
     }
+
   }
   let controller = {
-    init(view, model){
+    init(view, model) {
       this.view = view
       this.model = model
       this.view.render(this.model.data)
-      window.eventHub.on('upload', ()=>{
+      this.bindEvents()
+      this.bindEventsHub()
+      this.getAllSongs()
+    },
+    getAllSongs(){
+      return this.model.find().then(() => {
+        this.view.render(this.model.data)
+      })
+    },
+    bindEvents(){
+      $(this.view.el).on('click','li',(e)=>{
+        let li = $(e.currentTarget)
+        li.addClass('active')
+          .siblings('.active').removeClass('active')
+      })
+    },
+    bindEventsHub(){
+      window.eventHub.on('upload', () => {
         this.view.clearActive()
       })
-      window.eventHub.on('create', (songData)=>{
+      window.eventHub.on('create', (songData) => {
         this.model.data.songs.push(songData)
         this.view.render(this.model.data)
       })
-      let query = new AV.Query('Song')
-      query.find().then((res)=>{
-        console.log(res)
-      })
-    }
+    },
   }
 
   controller.init(view, model)
