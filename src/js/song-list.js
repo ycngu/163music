46423e543e1e8d0f3bd59ab8e9a227ceaae445a6
ycngu <snl123698745@163.com>
2019-01-25 22:59:59 +0800

    点击新建歌曲清空form

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -83,6 +83,9 @@
         this.model.data.songs.push(songData)
         this.view.render(this.model.data)
       })
+      window.eventHub.on('new',()=>{
+        this.view.clearActive()
+      })
     },
   }
 
