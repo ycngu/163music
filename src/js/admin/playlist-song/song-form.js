7f25d8d9fd6af35891da61b8341d2a9c22c4dd12
ycngu <snl123698745@163.com>
2019-03-26 00:00:10 +0800

    change way to fetch songs

--- src/js/admin/playlist-song/song-form.js
+++ src/js/admin/playlist-song/song-form.js
@@ -56,6 +56,7 @@
       leftSongId: null,
       rightSongId: null,
       listId: null,
+      ringtsongsId: null,
     },
     findSongs() {
       var query = new AV.Query('Song')
@@ -72,23 +73,56 @@
     findCurrentSongs(listId) {
       if (!listId) return
       var playlist = AV.Object.createWithoutData('PlayList', listId);
-      var query = new AV.Query('Song');
-      query.equalTo('dependent', playlist);
-      return query.find().then((songs) => {
-        this.data.rightSongs = songs.map((song) => {
-          return {
-            id: song.id,
-            ...song.attributes
-          }
+      // 构建 ListSongMap 的查询
+      var query = new AV.Query('ListSongMap');
+
+      // 查询所有选择了playlist的
+      query.equalTo('playlist', playlist);
+
+      // 执行查询
+
+      return query.find().then((lists) => {
+        let songs = []
+
+        songs = lists.map((list) => {
+          return list.attributes.song
         })
-        return songs
+
+        AV.Object.fetchAll(songs).then(function (objects) {
+          // 成功
+          console.log('object',objects)
+
+          this.data.rightSongs = objects.map((song)=>{
+            console.log('songggggggggg',song)
+            return {
+              id:song.id,
+              ...attributes
+            }
+          })
+          console.log('this.model.data.rightSongs',this.model.data.rightSongs)
+        }, function (error) {
+          // 异常处理
+          console.log('zzzzzzzzzzz',error)
+        });
+
+        return lists
       })
     },
-    setPointer(listId,songId){
-      var playlist = AV.Object.createWithoutData('PlayList', listId);
-      var song = AV.Object.createWithoutData('Song', songId);
-      song.set('dependent', playlist)
-      song.save()
+    setPointer(listId, songId) {
+      if (!listId) return
+      if (!songId) return
+      let song = AV.Object.createWithoutData('Song', songId);
+      let playlist = AV.Object.createWithoutData('PlayList', listId);
+
+      let listSongMAP = new AV.Object('ListSongMap');
+
+      // 设置关联
+      listSongMAP.set('song', song);
+      listSongMAP.set('playlist', playlist);
+
+      // listSongMAP.set('platform', 'web');
+
+      listSongMAP.save();
     }
   }
   let controller = {
@@ -108,23 +142,25 @@
       })
     },
     bindEvents() {
-      $(this.view.el).on('click','.lsonglist>li',(e)=>{
+      $(this.view.el).on('click', '.lsonglist>li', (e) => {
         let songId = e.currentTarget.getAttribute('data-song-id')
-        console.log('songid',songId)
+        console.log('songid', songId)
         this.model.data.leftSongId = songId
         this.view.render(this.model.data)
       })
 
 
-      $(this.view.el).on('click','#add',(e)=>{
+      $(this.view.el).on('click', '#add', (e) => {
         let songId = $(this.view.el).find('.lsonglist>li.active').attr('data-song-id')
         let listId = this.model.data.listId
-        this.model.setPointer(listId,songId)
-        this.model.findCurrentSongs(listId).then(()=>{
+
+        this.model.setPointer(listId, songId)
+
+        this.model.findCurrentSongs(listId).then(() => {
           this.view.render(this.model.data)
         })
       })
-      $(this.view.el).on('click','#remove',(e)=>{
+      $(this.view.el).on('click', '#remove', (e) => {
 
       })
     },
@@ -133,9 +169,9 @@
       //   this.model.data.songs.push(songData)
       //   this.view.render(this.model.data)
       // })
-      window.eventHub.on('selectxx',(data)=>{
+      window.eventHub.on('selectxx', (data) => {
         this.model.data.listId = data.id
-        this.model.findCurrentSongs(data.id).then(()=>{
+        this.model.findCurrentSongs(data.id).then(() => {
           this.view.render(this.model.data)
         })
       })
