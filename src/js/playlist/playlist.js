6595e5ad550161989c79f086c0b7e4e4fe5eabda
ycngu <snl123698745@163.com>
2019-04-02 23:48:54 +0800

    add css

--- src/js/playlist/playlist.js
+++ src/js/playlist/playlist.js
@@ -1,115 +1,116 @@
-
 {
-    let view = {
-        el: "section.songs",
-        template:`
-        <li>
-        <h3>{{song.name}}</h3>
-        <p>
-          <svg class="icon icon-sq">
-            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-sq"></use>
-          </svg>
-    {{song.singer}}
-        </p>
-        <a class="playButton" href="./song.html?id={{song.id}}">
-          <svg class="icon icon-play">
-            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-play"></use>
-          </svg>
-        </a>
-      </li>
-        `,
-        render(data){
-           
-            let {songs} =  data
-            songs.map((song)=>{
-                let $li = $(this.template
-                    .replace('{{song.name}}',song.name)
-                    .replace('{{song.singer}}',song.singer)
-                    .replace('{{song.id}}',song.id)
-                )
+  let view = {
+    el: "section.songs",
+    template: `
+    <li class="song-all" data-song-id="{{song.id}}">
+            <div class="order-number">{{number}}</div>
+            <div class="sgi_fr">
+                <div class="sgich_fl">
+                    <div class="f-thide sgich_tl">{{song.name}}</div>
+                    <div class="f-thide sgich_info">{{song.singer}} - {{song.name}}</div>
+                </div>
+                <div class="sgich_fr"><svg class="icon icon-play">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-play"></use>
+                  </svg></div>
+            </div>
+        </li>
+    `,
+    render(data) {
+      let {
+        songs
+      } = data
+      console.log(data)
+      songs.forEach((song,i) => {
+        console.log('song',song)
+        let $li = $(this.template
+          .replace('{{song.name}}', song.name)
+          .replace('{{song.singer}}', song.singer)
+          .replace('{{song.id}}', song.id)
+          .replace('{{number}}', i+1)
+          .replace('{{song.name}}', song.name)
+        )
 
-                $(this.el).find('ol.list').append($li)
-            })
-        }
+        $(this.el).find('ol.songlist').append($li)
+      })
     }
+  }
 
-    let model = {
-        data:{
-            id:'',
-            songs:[],
-            song:[],
-        },
-        findCurrentSongs(listId) {
-            if (!listId) return
-            let playlist = AV.Object.createWithoutData('PlayList', listId);
-            // 构建 ListSongMap 的查询
-            let query = new AV.Query('ListSongMap');
-            // 查询所有选择了playlist的
-            query.equalTo('playlist', playlist);
-      
-            // 执行查询
-            return query.find().then((lists) => {
-              let songs = []
-      
-              songs = lists.map((list) => {
-                return list.attributes.song
-              })
-      
+  let model = {
+    data: {
+      id: '',
+      songs: [],
+    },
+    findCurrentSongs(listId) {
+      if (!listId) return
+      let playlist = AV.Object.createWithoutData('PlayList', listId);
+      // 构建 ListSongMap 的查询
+      let query = new AV.Query('ListSongMap');
+      // 查询所有选择了playlist的
+      query.equalTo('playlist', playlist);
 
-              AV.Object.fetchAll(songs).then((s) => {
-                // 成功
+      // 执行查询
+      return query.find().then((lists) => {
+        let songs = []
 
-                this.data.songs = s.map((song) => {
-                  return {
-                    id: song.id,
-                    ...song.attributes
-                  }
-                })
-              }, function (error) {
-                // 异常处理
-                console.log('error', error)
-              });
+        songs = lists.map((list) => {
+          return list.attributes.song
+        })
 
-              return songs
-            })
-          },
-          getListId() {
-            let search = window.location.search
 
-            //去掉问号
-            if (search.indexOf('?') === 0) {
-                search = search.substring(1)
+        AV.Object.fetchAll(songs).then((s) => {
+          // 成功
+
+          this.data.songs = s.map((song) => {
+            return {
+              id: song.id,
+              ...song.attributes
             }
+          })
+        }, function (error) {
+          // 异常处理
+          console.log('error', error)
+        });
 
-            let array = search.split('&').filter((v => v))
-            let id = ''
+        return songs
+      })
+    },
+    getListId() {
+      let search = window.location.search
 
-            array.forEach((current) => {
-                let [key, value] = current.split('=')
-                if (key === 'id') {
-                    id = value
-                    return false
-                }
-            })
+      //去掉问号
+      if (search.indexOf('?') === 0) {
+        search = search.substring(1)
+      }
 
-            return id
-        },
-    }
+      let array = search.split('&').filter((v => v))
+      let id = ''
 
-    let controller = {
-        init(view,model){
-            this.view = view
-            this.model = model
-            this.model.id = this.model.getListId()
-            this.model.findCurrentSongs(this.model.id).then(()=>{
-                setTimeout(() => {
-                  this.view.render(this.model.data)
-                }, 500);
-                setTimeout(() => {
-                  this.view.render(this.model.data)
-                }, 1000);
-            })
-        },
-    }
-    controller.init(view, model)
+      array.forEach((current) => {
+        let [key, value] = current.split('=')
+        if (key === 'id') {
+          id = value
+          return false
+        }
+      })
+
+      return id
+    },
+  }
+
+  let controller = {
+    init(view, model) {
+      this.view = view
+      this.model = model
+      this.model.id = this.model.getListId()
+      this.model.findCurrentSongs(this.model.id).then(() => {
+        setTimeout(() => {
+          this.view.render(this.model.data)
+        }, 500);
+        // setTimeout(() => {
+        //   this.view.render(this.model.data)
+        // }, 1000);
+      })
+    },
+  }
+  controller.init(view, model)
 }
